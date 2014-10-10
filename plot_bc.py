5ac1de1c474760887150ecf186d00fdc79638e70
vmerkin <slava.merkin@jhuapl.edu>
2014-10-10 12:02:52 -0400

    Unimportant minor commits.

--- plot_bc.py
+++ plot_bc.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 
 from pylab import *
 data = loadtxt(args.bcFileName)
-
+"""
 figure()
 va = sqrt(data[:,2]**2+data[:,3]**2+data[:,4]**2)/sqrt(4.*pi*data[:,8])
 vf = sqrt(va**2+data[:,9]**2)
@@ -22,3 +22,4 @@ Mf = sqrt(data[:,5]**2+data[:,6]**2+data[:,7]**2)/vf
 pcolormesh(data[:,8].reshape(args.nj,args.nk))
 colorbar()
 show()
+"""

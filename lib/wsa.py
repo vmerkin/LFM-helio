9cff055bd56e4768308ff403a77e90b885683978
vmerkin <slava.merkin@jhuapl.edu>
2014-10-15 08:18:41 -0600

    Adding a simple info function
    The info function simply prints fits headers. It is called automatically if the module is accessed through __main__

--- lib/wsa.py
+++ lib/wsa.py
@@ -1,9 +1,9 @@
 import pyfits
-from numpy import linspace,pi,meshgrid,sin,cos,zeros,ones,dstack,diff,sqrt,array,savetxt,flatnonzero,insert,asarray,zeros_like
+from numpy import linspace,pi,meshgrid,sin,cos,zeros,ones,dstack,diff,sqrt,array,savetxt,flatnonzero,insert,asarray,zeros_like,argmin,unravel_index
 
 def read(wsa_file,densTempInfile,normalized=False):
     hdulist = pyfits.open(wsa_file)
-    print hdulist[0].header.ascardlist()
+    print hdulist[0].header.cards
     
     n_phi_wsa_v = hdulist[0].header['NAXIS1']+1  # number of cell vertices
     n_phi_wsa_c = hdulist[0].header['NAXIS1']    # number of cell centers
@@ -24,10 +24,29 @@ def read(wsa_file,densTempInfile,normalized=False):
         n_wsa = 112.64+9.49e7/v_wsa**2
         T0 = 8e5
         n0 = 300.
+        
+        B0 = bi_wsa[unravel_index(argmin(abs(n_wsa-n0)),n_wsa.shape)] # this is in nT
+        n0 = n_wsa[unravel_index(argmin(abs(n_wsa-n0)),n_wsa.shape)]  # this is in cm^-3
+
         T_wsa = n0*T0/n_wsa
+#        T_wsa = n0*T0/n_wsa + (B0**2-bi_wsa**2)/1.38/8./pi*1.e6/n_wsa
     hdulist.close()
 
     if normalized:
         return(phi_wsa_v,theta_wsa_v,phi_wsa_c,theta_wsa_c,bi_wsa,v_wsa,n_wsa*1.67e-24,T_wsa)
     else:
         return(phi_wsa_v,theta_wsa_v,phi_wsa_c,theta_wsa_c,bi_wsa*1.e-5,v_wsa*1.e5,n_wsa*1.67e-24,T_wsa)
+
+def info(wsa_file):
+    hdulist = pyfits.open(wsa_file)
+    print hdulist[0].header.cards
+    hdulist.close()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('wsaFile',help='WSA file to use')
+    args = parser.parse_args()
+    
+    info(args.wsaFile)
+    
